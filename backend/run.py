ed68322230d91359bbc3709d091324a254430ee5
Shritant <shritan06@gmail.com>
2025-06-02 17:48:31 -0400

    Set up basic Flask server

--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,16 @@
-print("Starting server")
+from flask import Flask
+
+app = Flask("Application")
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+app.run(port=4000,debug=True)
+
+print("Hello")
+
+
 
 
 
