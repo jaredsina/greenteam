b69bdeea3ccf7b7465bf5231fd86244fa25e65fa
Shritant <shritan06@gmail.com>
2025-06-30 18:03:25 -0400

    Polish Home Page
    Added and styled icons onto buttons and fixed the padding on the searchbar.

--- study_app_client/lib/main.dart
+++ study_app_client/lib/main.dart
@@ -98,147 +98,180 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Text("Hello, \n Nico", style: TextStyle(fontSize: 50)),
-            SearchBar(
-              leading: const Icon(Icons.search),
-              padding: const WidgetStatePropertyAll(
-                EdgeInsets.symmetric(horizontal: 16),
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            //
+            // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+            // action in the IDE, or press "p" in the console), to see the
+            // wireframe for each widget.
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Text("Hello, \n Nico!", style: TextStyle(fontSize: 50)),
+              SearchBar(
+                leading: const Icon(Icons.search),
+                padding: const WidgetStatePropertyAll(
+                  EdgeInsets.symmetric(horizontal: 16),
+                ),
               ),
-            ),
-            SizedBox(height: 20),
+              SizedBox(height: 20),
 
-            // Topic Buttons
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                FilledButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/study');
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: WidgetStateProperty.all(
-                      Color.fromARGB(255, 38, 0, 255),
-                    ),
-                    minimumSize: WidgetStateProperty.all(Size(150, 150)),
-                    shape: WidgetStateProperty.all(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
+              // Topic Buttons
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  FilledButton(
+                    // Free Hours Page
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/study');
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all(
+                        Color.fromARGB(255, 38, 0, 255),
+                      ),
+                      minimumSize: WidgetStateProperty.all(Size(150, 150)),
+                      shape: WidgetStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
                       ),
                     ),
-                  ),
-                  child: const Text('Free Hours'),
-                ),
-
-                FilledButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/study');
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: WidgetStateProperty.all(
-                      Color.fromARGB(255, 18, 12, 96),
-                    ),
-                    minimumSize: WidgetStateProperty.all(Size(150, 150)),
-                    shape: WidgetStateProperty.all(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                    ),
-                  ),
-                  child: const Text('Notes'),
-                ),
-              ],
-            ),
-
-            SizedBox(height: 20),
-            SizedBox(width: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                FilledButton(
-                  // Study Tools Button
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/study');
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: WidgetStateProperty.all(
-                      Color.fromARGB(255, 106, 173, 249),
-                    ),
-                    minimumSize: WidgetStateProperty.all(Size(150, 150)),
-                    shape: WidgetStateProperty.all(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
+                    child: Column(
+                      spacing: 12,
+                      children: [
+                        Text('Free Hours', style: TextStyle(fontSize: 20)),
+                        const Icon(Icons.schedule, size: 28),
+                      ],
                     ),
                   ),
 
-                  child: const Text('Study Tools'),
-                ),
-
-                FilledButton(
-                  // AI Helper Button
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/study');
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: WidgetStateProperty.all(
-                      Color.fromARGB(255, 0, 103, 182),
-                    ),
-                    minimumSize: WidgetStateProperty.all(Size(150, 150)),
-                    shape: WidgetStateProperty.all(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
+                  FilledButton(
+                    // Notes Page
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/study');
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all(
+                        Color.fromARGB(255, 18, 12, 96),
                       ),
+                      minimumSize: WidgetStateProperty.all(Size(150, 150)),
+                      shape: WidgetStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                    ),
+                    child: Column(
+                      spacing: 12,
+                      children: [
+                        Text('Notes', style: TextStyle(fontSize: 25)),
+                        const Icon(Icons.auto_stories, size: 28),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+
+              SizedBox(height: 20),
+              SizedBox(width: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  FilledButton(
+                    // Study Tools Button
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/study');
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all(
+                        Color.fromARGB(255, 0, 166, 237),
+                      ),
+                      minimumSize: WidgetStateProperty.all(Size(150, 150)),
+                      shape: WidgetStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                    ),
+                    child: Column(
+                      spacing: 12,
+                      children: [
+                        Text('Study Tools', style: TextStyle(fontSize: 20)),
+                        const Icon(Icons.design_services, size: 28),
+                      ],
                     ),
                   ),
 
-                  child: const Text('AI Helper'),
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            SizedBox(width: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                FilledButton(
-                  // Schedule Button
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/schedule');
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: WidgetStateProperty.all(
-                      Color.fromARGB(255, 0, 166, 237),
-                    ),
-                    minimumSize: WidgetStateProperty.all(Size(320, 60)),
-                    shape: WidgetStateProperty.all(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
+                  FilledButton(
+                    // AI Helper Button
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/study');
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all(
+                        Color.fromARGB(255, 0, 103, 182),
+                      ),
+                      minimumSize: WidgetStateProperty.all(Size(150, 150)),
+                      shape: WidgetStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
                       ),
                     ),
+                    child: Column(
+                      spacing: 12,
+                      children: [
+                        Text('AI Helper', style: TextStyle(fontSize: 25)),
+                        const Icon(Icons.smart_toy, size: 28),
+                      ],
+                    ),
                   ),
-                  child: const Text('Schedule'),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+              SizedBox(height: 20),
+              SizedBox(width: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  FilledButton(
+                    // Schedule Button
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/schedule');
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all(
+                        Color.fromARGB(255, 106, 173, 249),
+                      ),
+                      minimumSize: WidgetStateProperty.all(Size(320, 60)),
+                      shape: WidgetStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                    ),
+                    child: Row(
+                      spacing: 16,
+                      children: [
+                        Text('Schedule', style: TextStyle(fontSize: 25)),
+                        const Icon(Icons.calendar_month, size: 28),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
