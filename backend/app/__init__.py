f06c425149f5694fcb7bdf9408b1f0176c7fd0a6
Ruhan Dutta <ruhan122011@gmail.com>
2025-08-25 18:00:56 -0400

    Installed CORS on the backed

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from .routes import init_routes
 from .config import init_config
-
+from flask_cors import CORS
 def create_app():
+
   print("Creating Flask Application")
   app = Flask("Application")
 
+  CORS(app, supports_credentials=True)
+
   init_routes(app)
   init_config(app)
+
+
   return app
   
 
