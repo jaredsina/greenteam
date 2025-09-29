d5f6a2794db3c656f022710c74a48f814a19919f
Shritant <shritan06@gmail.com>
2025-09-29 17:59:17 -0400

    Change the way we grab data in request

--- backend/app/routes/user_routes.py
+++ backend/app/routes/user_routes.py
@@ -6,8 +6,9 @@ user_routes = Blueprint("user_routes", __name__)
 @user_routes.route("/create", methods=['POST'])
 def create_users():
   try:
-    username = request.form.get("username")
-    password = request.form.get("password")
+    data = request.get_json()
+    username = data["username"]
+    password = data["password"]
 
     user_model = UserModel(current_app.mongo)
     response = user_model.create_user(
@@ -15,6 +16,7 @@ def create_users():
       password,
     )
   except Exception as e:
+        print(e)
         return jsonify({"message": "Error creating user", "error": str(e)}), 400
 
   return jsonify(
