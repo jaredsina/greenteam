38255d04171a42841bf0db39c22fcc1fa10dda87
Shritant <shritan06@gmail.com>
2025-10-20 17:51:21 -0400

    Update home.dart

--- study_app_client/lib/Pages/home.dart
+++ study_app_client/lib/Pages/home.dart
@@ -10,62 +10,42 @@ class Home extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<Home> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Color.fromARGB(255, 44, 153, 237),
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
         child: Padding(
           padding: EdgeInsets.all(16),
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
           child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            //
-            // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-            // action in the IDE, or press "p" in the console), to see the
-            // wireframe for each widget.
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              Text("Hello, \n Nico!", style: TextStyle(fontSize: 50)),
+              Text("Hello,\nJanav!", style: TextStyle(fontSize: 50)),
+              SizedBox(height: 20),
               SearchBar(
                 leading: const Icon(Icons.search),
                 padding: const WidgetStatePropertyAll(
                   EdgeInsets.symmetric(horizontal: 16),
                 ),
               ),
-              SizedBox(height: 20),
-
-              // Topic Buttons
+              SizedBox(height: 60),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   FilledButton(
-                    // Free Hours Page
                     onPressed: () {
-                      Navigator.pushNamed(context, '/study');
+                      Navigator.pushNamed(context, '/schedule');
                     },
                     style: ButtonStyle(
                       backgroundColor: WidgetStateProperty.all(
@@ -81,16 +61,14 @@ class _MyHomePageState extends State<Home> {
                     child: Column(
                       spacing: 12,
                       children: [
-                        Text('Free Hours', style: TextStyle(fontSize: 20)),
+                        Text('Schedule', style: TextStyle(fontSize: 20)),
                         const Icon(Icons.schedule, size: 28),
                       ],
                     ),
                   ),
-
                   FilledButton(
-                    // Notes Page
                     onPressed: () {
-                      Navigator.pushNamed(context, '/study');
+                      Navigator.pushNamed(context, '/notes');
                     },
                     style: ButtonStyle(
                       backgroundColor: WidgetStateProperty.all(
@@ -113,14 +91,11 @@ class _MyHomePageState extends State<Home> {
                   ),
                 ],
               ),
-
               SizedBox(height: 20),
-              SizedBox(width: 20),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   FilledButton(
-                    // Study Tools Button
                     onPressed: () {
                       Navigator.pushNamed(context, '/study');
                     },
@@ -143,9 +118,7 @@ class _MyHomePageState extends State<Home> {
                       ],
                     ),
                   ),
-
                   FilledButton(
-                    // AI Helper Button
                     onPressed: () {
                       Navigator.pushNamed(context, '/study');
                     },
@@ -165,37 +138,6 @@ class _MyHomePageState extends State<Home> {
                       children: [
                         Text('AI Helper', style: TextStyle(fontSize: 25)),
                         const Icon(Icons.smart_toy, size: 28),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20),
-              SizedBox(width: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  FilledButton(
-                    // Schedule Button
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/schedule');
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: WidgetStateProperty.all(
-                        Color.fromARGB(255, 106, 173, 249),
-                      ),
-                      minimumSize: WidgetStateProperty.all(Size(320, 60)),
-                      shape: WidgetStateProperty.all(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                      ),
-                    ),
-                    child: Row(
-                      spacing: 16,
-                      children: [
-                        Text('Schedule', style: TextStyle(fontSize: 25)),
-                        const Icon(Icons.calendar_month, size: 28),
                       ],
                     ),
                   ),
